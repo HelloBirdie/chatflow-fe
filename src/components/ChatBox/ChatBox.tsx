4abe2fb4fc56682db87eb5987b1b33d9952fef23
JackNeedFocus <Jack.z.deng@outlook.com>
2023-05-02 12:38:33 +0930

    add: chatbox container

---
+++ src/components/ChatBox/ChatBox.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ChatBox = () => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '60px',
+        right: '20px',
+        width: '400px',
+        height: '70vh',
+        backgroundColor: 'white',
+        border: '1px solid black',
+        zIndex: 1000,
+      }}
+    >
+      ChatBox
+    </div>
+  );
+};
+
+export default ChatBox;
--- src/pages/Mindmap/Mindmap.tsx
+++ src/pages/Mindmap/Mindmap.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MindmapCanvas from '@/components/MindmapCanvas/MindmapCanvas';
+import ChatBox from '../../components/ChatBox/ChatBox';
 
 const Mindmap = () => {
   return (
     <div>
+      <ChatBox />
       <MindmapCanvas />
     </div>
   );

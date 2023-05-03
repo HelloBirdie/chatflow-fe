3f662481d63bb403ac7127605713e66bdb0d0734
Jack Deng <Jack.z.deng@outlook.com>
2023-05-03 11:03:17 +0930

    Chat 16 react chat widget demo (#6)
    * add: chatbox container
    
    * update: add button to toggle the minimap
    
    * add: basic layout of mindmap
    
    * add: chatbox animation
    
    * add: animation

---
+++ src/components/MindMapHeaderBar/MindMapHeaderBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MindMapHeaderBar = () => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: '0px',
+        left: '0px',
+        width: '100vw',
+        height: '60px',
+        backgroundColor: 'white',
+        border: '1px solid black',
+        zIndex: 1000,
+      }}
+    >
+      MindMapHeaderBar
+    </div>
+  );
+};
+
+export default MindMapHeaderBar;
--- src/pages/Mindmap/Mindmap.tsx
+++ src/pages/Mindmap/Mindmap.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import MindmapCanvas from '@/components/MindmapCanvas/MindmapCanvas';
+import ChatBox from '../../components/ChatBox/ChatBox';
+
+import MindMapHeaderBar from '../../components/MindMapHeaderBar/MindMapHeaderBar';
 
 const Mindmap = () => {
   return (
     <div>
+      <MindMapHeaderBar />
+      <ChatBox />
       <MindmapCanvas />
     </div>
   );

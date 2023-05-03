23f10463a7b80ef3c9afb1935cd0d1e19ab7b50f
JackNeedFocus <Jack.z.deng@outlook.com>
2023-05-03 19:01:58 +0930

    feat: the ui of the chat input box

--- src/components/ChatMessageInput/ChatMessageInput.tsx
+++ src/components/ChatMessageInput/ChatMessageInput.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import {
+  Flex,
+  Textarea,
+  InputGroup,
+  IconButton,
+  Icon,
+  Spacer,
+} from '@chakra-ui/react';
+
+import ResizeTextarea from 'react-textarea-autosize';
+
+import { FiSend } from 'react-icons/fi';
 
 const ChatMessageInput = () => {
-  return <div>ChatMessageInput</div>;
+  const [message, setMessage] = useState('');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleMessageChange = (event: any) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    // Add your logic to send the message here
+    console.log('Message sent:', message);
+    setMessage('');
+  };
+
+  return (
+    <Flex padding="3" pr="2">
+      <InputGroup display={'flex'} alignItems={'end'}>
+        <Textarea
+          minH="unset"
+          mr={1}
+          ref={textareaRef}
+          maxRows={8}
+          bg={'gray.100'}
+          variant={'none'}
+          resize={'none'}
+          placeholder="Type your message here..."
+          value={message}
+          onChange={handleMessageChange}
+          as={ResizeTextarea}
+        />
+        <IconButton
+          bg={'transparent'}
+          size={'md'}
+          icon={<Icon as={FiSend} />}
+          onClick={handleSendMessage}
+          aria-label="Send message"
+        />
+      </InputGroup>
+    </Flex>
+  );
 };
 
 export default ChatMessageInput;

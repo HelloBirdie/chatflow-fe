82618a970291d1a583627c214066f8db295acb18
JackNeedFocus <Jack.z.deng@outlook.com>
2023-05-08 23:31:38 +0930

    feat: customized node

---
+++ src/components/MindmapNode/MindmapNode.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NodeContainer = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  border-radius: 8px;
+`;
+
+interface MindmapNodeProps {
+  data: {
+    conversationPairId: number;
+    userMessage: string;
+    aiMessage: string;
+  };
+}
+
+const MindmapNode = ({ data }: MindmapNodeProps) => {
+  console.log(data);
+  return (
+    <NodeContainer>
+      <p>{data.userMessage}</p>
+      <hr />
+      <p>{data.aiMessage}</p>
+    </NodeContainer>
+  );
+};
+
+export default MindmapNode;

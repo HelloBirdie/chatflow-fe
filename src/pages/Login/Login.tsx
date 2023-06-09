4eef182382f8a5dc01ac3170fab7c05b48e5f91d
JackNeedFocus <Jack.z.deng@outlook.com>
2023-06-09 22:25:09 +0930

    add: email login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 
 import logo from '@/assets/images/chatflow-logo-round-blue-bg.png';
 import { Link } from 'react-router-dom';
-import { Icon } from '@chakra-ui/react';
+import { Icon, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 import { BsGithub } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
-import { Input } from '@chakra-ui/react';
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from '@chakra-ui/react';
 import clsx from 'clsx';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    alert(`Email: ${email} & Password: ${password}`);
+  };
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center -mt-20">
       <div className="flex flex-col items-center">
         {/* Logo */}
         <img
@@ -57,6 +57,36 @@ const Login = () => {
           <span className="flex-grow-0 mx-4 ">Or with your email</span>
           <div className="flex-grow bg bg-gray-300 h-[1px]" />
         </p>
+
+        {/* Login Form */}
+        <div>
+          <form onSubmit={handleSubmit}>
+            <FormControl mt={6} isRequired>
+              <Input
+                type="email"
+                placeholder="Your email"
+                width={80}
+                size="lg"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </FormControl>
+            <FormControl mt={4} isRequired>
+              <Input
+                type="password"
+                placeholder="Password"
+                size="lg"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </FormControl>
+            <Button width="full" mt={4} type="submit">
+              Sign In
+            </Button>
+          </form>
+        </div>
+
+        <Link to="/signup" className="text-gray-400 hover:underline mt-3">
+          Forgot password?
+        </Link>
       </div>
     </div>
   );

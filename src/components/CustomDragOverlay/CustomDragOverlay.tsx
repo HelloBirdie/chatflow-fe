caffe96a63ba791816fcdc48ce65e1145ae384a5
Charlotte Lai <laiyiyihan0315@gmail.com>
2023-07-24 13:49:32 +0800

    Edit: conversation pair drag styles

--- src/components/CustomDragOverlay/CustomDragOverlay.tsx
+++ src/components/CustomDragOverlay/CustomDragOverlay.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import ConversationPair from '../ConversationPair/ConversationPair';
 import { IConversationPair } from '@/interfaces/conversationPair';
+import { Card, CardBody, VStack, HStack, Icon } from '@chakra-ui/react';
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
+import { BsChatSquareDots } from 'react-icons/bs';
 
-const Wrapper = styled.div`
+const CardContainer = styled.div`
   position: absolute;
+  opacity: 0.8;
   z-index: 2000;
-  border: 1px solid black;
-  border-radius: 8px;
-  padding: 10px;
-  cursor: default;
+  rotate: -5deg;
 
-  &.not-in-chatbox {
-    border: 3px solid red;
+  .card {
+    padding: 10px;
+  }
+
+  .not-in-chatbox {
     cursor: copy;
+  }
+
+  .message-skeleton {
+    width: 120px;
+    padding: 5px;
+    border-radius: 5px;
+    background-color: #fafafa;
   }
 `;
 
@@ -34,11 +45,33 @@ const CustomDragOverlay = ({
   const userMessageContent = userMessage.content;
   const aiMessageContent = aiMessage.content;
   return (
-    <Wrapper className={inChatBox ? '' : 'not-in-chatbox'}>
-      <div>Question: {userMessageContent}</div>
-      <div>Answer: {aiMessageContent}</div>
-      {!inChatBox ? <div>Add to mindmap</div> : null}
-    </Wrapper>
+    <CardContainer>
+      <Card className={inChatBox ? 'card' : 'card not-in-chatbox'}>
+        <VStack>
+          <HStack className="message-skeleton">
+            <Icon
+              className="message-icon"
+              as={AiOutlineQuestionCircle}
+              w={3}
+              h={3}
+              color="#0042D9"
+            />
+          </HStack>
+          <HStack className="message-skeleton">
+            <Icon
+              className="message-icon"
+              as={BsChatSquareDots}
+              w={3}
+              h={3}
+              color="#2BA245"
+            />
+          </HStack>
+        </VStack>
+        {/* <div>Question: {userMessageContent}</div>
+          <div>Answer: {aiMessageContent}</div>
+          {!inChatBox ? <div>Add to mindmap</div> : null} */}
+      </Card>
+    </CardContainer>
   );
 };
 
